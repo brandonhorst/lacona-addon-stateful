5de9c7c756bd0579d89986a6c392b6b9915b8e3b
Brandon Horst <brandonhorst@gmail.com>
2015-01-07 10:34:07 -0500

    Fixed for async parses

--- test/test.js
+++ test/test.js
@@ -7,13 +7,6 @@ var Stateful = require('..');
 
 var expect = chai.expect;
 
-var grammar = {
-	phrases: [{
-		name: 'test',
-		root: 'test'
-	}]
-};
-
 function toStream(strings) {
 	var newStream = new stream.Readable({objectMode: true});
 
@@ -42,54 +35,116 @@ function toArray(done) {
 describe('lacona-addon-stateful', function () {
 	var parser, stateful;
 
-	beforeEach(function () {
-		parser = new lacona.Parser({sentences: ['test']}).understand(grammar);
-		stateful = new Stateful({serializer: fulltext});
+	describe('syncronous parse', function () {
+		beforeEach(function () {
+			var grammar = {
+				phrases: [{
+					name: 'test',
+					root: 'test'
+				}]
+			};
+
+			parser = new lacona.Parser({sentences: ['test']}).understand(grammar);
+			stateful = new Stateful({serializer: fulltext});
+		});
+
+		it('emits insert for first occurrence of data' , function (done) {
+			function callback(data) {
+				expect(data).to.have.length(1);
+				expect(data[0].event).to.equal('insert');
+				expect(data[0].data.suggestion.words[0].string).to.equal('test');
+				expect(data[0].id).to.equal(0);
+				done();
+			}
+
+			toStream(['t'])
+				.pipe(parser)
+				.pipe(stateful)
+				.pipe(toArray(callback));
+		});
+
+		it('emits an update if the input changes' , function (done) {
+			function callback(data) {
+				expect(data).to.have.length(2);
+				expect(data[0].event).to.equal('insert');
+				expect(data[1].event).to.equal('update');
+				expect(data[0].data.suggestion.words[0].string).to.equal('test');
+				expect(data[1].data.suggestion.words[0].string).to.equal('test');
+				expect(data[0].id).to.equal(data[1].id);
+				done();
+			}
+
+			toStream(['t', 'te'])
+				.pipe(parser)
+				.pipe(stateful)
+				.pipe(toArray(callback));
+		});
+
+		it('emits a delete if a valid option goes away' , function (done) {
+			function callback(data) {
+				expect(data).to.have.length(2);
+				expect(data[0].event).to.equal('insert');
+				expect(data[1].event).to.equal('delete');
+				expect(data[0].data.suggestion.words[0].string).to.equal('test');
+				done();
+			}
+
+			toStream(['t', 'tx'])
+				.pipe(parser)
+				.pipe(stateful)
+				.pipe(toArray(callback));
+		});
 	});
 
-	it('emits insert for first occurrence of data' , function (done) {
-		function callback(data) {
-			expect(data).to.have.length(1);
-			expect(data[0].event).to.equal('insert');
-			expect(data[0].data.suggestion.words[0].string).to.equal('test');
-			done();
-		}
+	describe('async parse', function () {
+		beforeEach(function () {
+			var grammar = {
+				scope: {
+					delay: function (input, data, done) {
+						setTimeout(function () {
+							data({display: 'test', value: 'test'});
+							done();
+						}, 0);
+					}
+				},
+				phrases: [{
+					name: 'test',
+					root: {
+						type: 'value',
+						compute: 'delay'
+					}
+				}]
+			};
 
-		toStream(['t'])
-			.pipe(parser)
-			.pipe(stateful)
-			.pipe(toArray(callback));
-	});
+			parser = new lacona.Parser({sentences: ['test']}).understand(grammar);
+			stateful = new Stateful({serializer: fulltext});
+		});
 
-	it('does not emit multiple events if parsed before handling' , function (done) {
-		function callback(data) {
-			expect(data).to.have.length(2);
-			expect(data[0].event).to.equal('insert');
-			expect(data[1].event).to.equal('update');
-			expect(data[0].data.suggestion.words[0].string).to.equal('test');
-			expect(data[1].data.suggestion.words[0].string).to.equal('test');
-			done();
-		}
+		it('does not emit anything if the most recent parse has no data', function (done) {
+			function callback(data) {
+				expect(data).to.have.length(0);
+				done();
+			}
 
-		toStream(['t', 'te'])
-			.pipe(parser)
-			.pipe(stateful)
-			.pipe(toArray(callback));
-	});
+			toStream(['t', 'tx'])
+				.pipe(parser)
+				.pipe(stateful)
+				.pipe(toArray(callback));
+		});
 
-	it('does not emit events if none are valid' , function (done) {
-		function callback(data) {
-			expect(data).to.have.length(2);
-			expect(data[0].event).to.equal('insert');
-			expect(data[1].event).to.equal('delete');
-			expect(data[0].data.suggestion.words[0].string).to.equal('test');
-			done();
-		}
+		it('only emits one event for async if the second comes before the first completes', function (done) {
+			function callback(data) {
+				expect(data).to.have.length(1);
+				expect(data[0].event).to.equal('insert');
+				expect(data[0].data.suggestion.charactersComplete).to.equal(2);
+				done();
+			}
 
-		toStream(['t', 'tx'])
-			.pipe(parser)
-			.pipe(stateful)
-			.pipe(toArray(callback));
+			toStream(['t', 'te'])
+				.pipe(parser)
+				.pipe(stateful)
+				.pipe(toArray(callback));
+		});
 	});
 
 });
